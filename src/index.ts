ac2e7957ebab9e8535a018d71972c8fbb6c9bdb1
Geoffrey Tsui <tsui.geoffrey@gmail.com>
2023-12-14 17:35:41 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { encodeFunctionData } from "viem";
-import ERC20 from "./abi/ERC20.js";
+import { encodeFunctionData } from 'viem';
+import ERC20 from './abi/ERC20.js';
 
 const main = () => {
   let data = encodeFunctionData({
     abi: ERC20,
-    functionName: "totalSupply",
+    functionName: 'totalSupply',
   });
   console.log(data);
 };
